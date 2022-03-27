d47261db3f316e7021aaa1c6a43b77c5d1f67e64
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-03-27 17:17:18 +0600

    PHP Sum = 27/03/2022

---
+++ SUM/String with Number.php
@@ -0,0 +1,22 @@
+
+
+
+
+<?php
+
+
+            //What is the type of variable if two strings are added?
+            
+            //01 First you have to write two strings below.
+
+
+    $num1 = '10';
+    $num2 = '20';
+
+    //note: (' ') If there is any text in an invited comma, it is called a string.
+
+    echo gettype ($num1 + $num2 );
+
+
+
+?>
---
+++ SUM/index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+// // How to Sum in PHP
+
+// 01. Fostly = Varyable Diclaration  
+//         Note - Minimum Varyable = 2 Data Type 
+
+// 02. Then => Echo 
+
+
+//     $number1 = 20;
+//     $number2 = 20;
+
+
+
+//     Ex:  echo $number1 +  $number2
+
+//     Ans: 40
+
+
+$number1 = 20;
+$number2 = 20;
+
+
+
+Ex:  echo $number1 +  $number2
+
+
+
+
+?>
